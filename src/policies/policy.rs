287572216025472ee652378b54a2f21e720f4e21
tubo213 <tubo213>
2024-08-26 10:29:59 +0900

    actionにto_jsonを追加

--- src/policies/policy.rs
+++ src/policies/policy.rs
@@ -9,6 +9,12 @@ pub struct Action {
     pub piece_index: usize,
 }
 
+impl Action {
+    pub fn to_json(&self) -> String {
+        serde_json::to_string(self).unwrap()
+    }
+}
+
 pub trait Policy {
     fn new() -> Self
     where
