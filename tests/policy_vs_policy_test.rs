fddc0ea92303527172b11d6617d218a541fbdc5e
tubo213 <tubo213>
2024-08-27 15:59:35 +0900

    greedy randomをone look aheadに変更

--- tests/policy_vs_policy_test.rs
+++ tests/policy_vs_policy_test.rs
@@ -1,23 +1,22 @@
 use quart_engine::game::Player;
-use quart_engine::policies::{Policy, RandomPolicy, GreedyRandomPolicy};
+use quart_engine::policies::{OneStepLookAheadPolicy, Policy, RandomPolicy};
 use quart_engine::runner::Runner;
 use tqdm::tqdm;
 
 const NUM_TRIALS: u64 = 10000;
 
 // 汎用的なテスト関数を定義
-fn test_policy_vs_policy<P1, P2>(
-    player1_policy: P1,
-    player2_policy: P2,
-    description: &str,
-)
+fn test_policy_vs_policy<P1, P2>(player1_policy: P1, player2_policy: P2, description: &str)
 where
     P1: Policy + Clone + 'static,
     P2: Policy + Clone + 'static,
 {
     let mut win_count = 0;
     for _ in tqdm(0..NUM_TRIALS) {
-        let mut runner = Runner::new(Box::new(player1_policy.clone()), Box::new(player2_policy.clone()));
+        let mut runner = Runner::new(
+            Box::new(player1_policy.clone()),
+            Box::new(player2_policy.clone()),
+        );
         let winner = runner.run();
         if let Some(Player::Player1) = winner {
             win_count += 1;
@@ -29,10 +28,18 @@ where
 
 #[test]
 fn test_random_vs_random_policy() {
-    test_policy_vs_policy(RandomPolicy::new(), RandomPolicy::new(), "Random vs Random Policy");
+    test_policy_vs_policy(
+        RandomPolicy::new(),
+        RandomPolicy::new(),
+        "Random vs Random Policy",
+    );
 }
 
 #[test]
-fn test_random_vs_greedy_random_policy() {
-    test_policy_vs_policy(GreedyRandomPolicy::new(), RandomPolicy::new(), "Greedy Random vs Random Policy");
+fn test_random_vs_one_step_look_ahead_policy() {
+    test_policy_vs_policy(
+        OneStepLookAheadPolicy::new(),
+        RandomPolicy::new(),
+        "One Step Look Ahead vs Random Policy",
+    );
 }
