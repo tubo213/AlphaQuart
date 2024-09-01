29af82f2e6831199143c5d58bb75d594fcd1b82e
tubo213 <tubo213>
2024-09-01 18:38:08 +0900

    expandを実装

--- src/policies/mcts_policy.rs
+++ src/policies/mcts_policy.rs
@@ -1,5 +1,34 @@
+use crate::game::action::Action;
 use crate::game::Game;
-use crate::policies::policy::{Action, Policy};
+use crate::policies::policy::Policy;
+
+struct Node {
+    state: Game,
+    reward: f64,
+    num_visits: u64,
+    children: Vec<Node>,
+}
+
+impl Node {
+    fn new(state: Game) -> Self {
+        Node {
+            state,
+            reward: 0.0,
+            num_visits: 0,
+            children: vec![],
+        }
+    }
+
+    fn expand(&mut self) {
+        for action in self.state.available_actions() {
+            let mut child_state = self.state.clone();
+            child_state
+                .play_turn(action.row, action.col, action.piece_index)
+                .unwrap();
+            self.children.push(Node::new(child_state));
+        }
+    }
+}
 
 #[derive(Clone)]
 pub struct MCTSPolicy {}
