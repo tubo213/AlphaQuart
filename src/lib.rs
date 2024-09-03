21164c6778ece0097192965cebf3145b4f016b96
tubo213 <tubo213>
2024-09-03 13:53:28 +0900

    mcsからtime keeperを利用できるように修正

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod game;
 pub mod policies;
 pub mod runner;
+pub mod utils;
--- src/utils.rs
+++ src/utils.rs
@@ -1,27 +1,20 @@
 // ref: https://zenn.dev/tipstar0125/articles/245bceec86e40a#time-keeper
 #[derive(Debug, Clone)]
-struct TimeKeeper {
+pub struct TimeKeeper {
     start_time: std::time::Instant,
     time_threshold: f64,
 }
 
 impl TimeKeeper {
-    fn new(time_threshold: f64) -> Self {
+    pub fn new(time_threshold: f64) -> Self {
         TimeKeeper {
             start_time: std::time::Instant::now(),
             time_threshold,
         }
     }
     #[inline]
-    fn isTimeOver(&self) -> bool {
+    pub fn is_time_over(&self) -> bool {
         let elapsed_time = self.start_time.elapsed().as_nanos() as f64 * 1e-9;
-        #[cfg(feature = "local")]
-        {
-            elapsed_time * 0.85 >= self.time_threshold
-        }
-        #[cfg(not(feature = "local"))]
-        {
-            elapsed_time >= self.time_threshold
-        }
+        elapsed_time >= self.time_threshold
     }
 }
