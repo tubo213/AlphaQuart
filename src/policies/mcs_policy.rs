eb9895b0f04ada218280559695a4bc7f8061a951
tubo213 <tubo213>
2024-09-03 14:48:26 +0900

    MCSで探索時間いっぱい探索するように改善

--- src/policies/mcs_policy.rs
+++ src/policies/mcs_policy.rs
@@ -3,61 +3,71 @@ use crate::game::Game;
 use crate::game::Player;
 use crate::policies::one_step_look_ahead_policy::OneStepLookAheadPolicy;
 use crate::policies::policy::Policy;
-
-pub fn play_out(game: &Game, policy: &OneStepLookAheadPolicy) -> Option<Player> {
-    let mut game_copy = game.clone();
-    while !game_copy.is_game_over() {
-        let action = policy.action(&game_copy);
-        game_copy
-            .play_turn(action.row, action.col, action.piece_index)
-            .unwrap();
-    }
-
-    game_copy.judge_winner()
-}
+use crate::utils::TimeKeeper;
 
 #[derive(Clone)]
 pub struct MCSPolicy {
-    num_playouts_per_action: usize,
-    policy: OneStepLookAheadPolicy,
+    pub policy: OneStepLookAheadPolicy,
+    pub max_time: f64,
 }
 
 impl Policy for MCSPolicy {
     fn new() -> Self {
         MCSPolicy {
-            num_playouts_per_action: 10,
             policy: OneStepLookAheadPolicy::new(),
+            max_time: 0.01,
         }
     }
 
     fn action(&self, game: &Game) -> Action {
-        // 渡せるpieceがない場合は、OneStepLookAheadPolicyを使う
         if game.available_pieces.is_empty() {
+            // 渡せる駒が無い場合は，置いて勝てる手があるならそれを選択すれば良い
+            // これはOneStepLookAheadPolicyのactionメソッドと同じ
             return self.policy.action(game);
         } else {
             // すべてのactionに対して、num_playouts_per_action回プレイアウトを行い、最も勝率が高いactionを選択
-            let mut best_action: Option<Action> = None;
-            let mut best_score = std::f64::MIN;
+            // 実行時間内でできるだけ多くのプレイアウトを行い，最も勝率が高い手を選択する
 
-            for action in game.available_actions().iter() {
-                let mut next_state = game.clone();
-                let mut score = 0.0;
-                next_state
-                    .play_turn(action.row, action.col, action.piece_index)
-                    .unwrap();
-                for _ in 0..self.num_playouts_per_action {
-                    // play out the game
+            // アクションを列挙して次の状態を計算
+            let available_actions = game.available_actions();
+            let n_available_actions = available_actions.len();
+            if n_available_actions == 0 {
+                panic!("利用可能なアクションがありません");
+            }
+            let next_states: Vec<Game> = available_actions
+                .iter()
+                .map(|action| {
+                    let mut next_state = game.clone();
+                    next_state
+                        .play_turn(action.row, action.col, action.piece_index)
+                        .unwrap();
+                    next_state
+                })
+                .collect();
+
+            // 時間いっぱいプレイアウトを行う
+            let mut time_keeper = TimeKeeper::new(self.max_time);
+            let mut scores = vec![0; n_available_actions];
+            let mut counts = vec![0; n_available_actions];
+            while !time_keeper.is_time_over() {
+                for (i, next_state) in next_states.iter().enumerate() {
                     let winner = play_out(&next_state, &self.policy);
                     if winner.is_some() {
                         if winner.unwrap() == game.current_player {
-                            score += 1.0;
+                            scores[i] += 1;
                         } else {
-                            score -= 1.0;
+                            scores[i] -= 1;
                         }
                     }
+                    counts[i] += 1;
                 }
+            }
 
-                score /= self.num_playouts_per_action as f64;
+            // // 平均scoreが最も高いactionを選択
+            let mut best_action: Option<Action> = None;
+            let mut best_score = std::f64::MIN;
+            for (i, action) in available_actions.iter().enumerate() {
+                let score = scores[i] as f64 / counts[i] as f64;
                 if score > best_score {
                     best_score = score;
                     best_action = Some(action.clone());
@@ -67,6 +77,18 @@ impl Policy for MCSPolicy {
             best_action.unwrap()
         }
     }
+}
+
+fn play_out(game: &Game, policy: &OneStepLookAheadPolicy) -> Option<Player> {
+    let mut game_copy = game.clone();
+    while !game_copy.is_game_over() {
+        let action = policy.action(&game_copy);
+        game_copy
+            .play_turn(action.row, action.col, action.piece_index)
+            .unwrap();
+    }
+
+    game_copy.judge_winner()
 }
 
 #[cfg(test)]
--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,7 @@
 pub struct TimeKeeper {
     start_time: std::time::Instant,
     time_threshold: f64,
+    count: u64,
 }
 
 impl TimeKeeper {
@@ -10,11 +11,17 @@ impl TimeKeeper {
         TimeKeeper {
             start_time: std::time::Instant::now(),
             time_threshold,
+            count: 0,
         }
     }
     #[inline]
-    pub fn is_time_over(&self) -> bool {
+    pub fn is_time_over(&mut self) -> bool {
+        self.count += 1;
         let elapsed_time = self.start_time.elapsed().as_nanos() as f64 * 1e-9;
         elapsed_time >= self.time_threshold
+    }
+
+    pub fn get_count(&self) -> u64 {
+        self.count
     }
 }
